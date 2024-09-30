3d11294d80054eccc5d43501481eeb284d623b97
Monique <nica14101@gmail.com>
2024-09-30 00:05:17 -0700

    finished repo set-up for first sprints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,26 @@ import BPLogo from '@/assets/images/bp-logo.png';
 
 export default function Home() {
   return (
-    <main>
-      <p>page :0</p>
+    <main style={mainStyles}>
+      <Image style={imageStyles} src={BPLogo} alt="Blueprint Logo" />
+      <p>Open up app/page.tsx to get started!</p>
     </main>
   );
 }
+
+// CSS styles
+
+const mainStyles: CSSProperties = {
+  width: '100%',
+  height: '100vh',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+const imageStyles: CSSProperties = {
+  width: '80px',
+  height: '80px',
+  marginBottom: '0.5rem',
+};
