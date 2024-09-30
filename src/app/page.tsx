6b12a66f44bfcfc67feae3028eb7a86c24da0e87
Monique <nica14101@gmail.com>
2024-09-30 00:03:48 -0700

    finished repo set-up for first sprints

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+import Image from 'next/image';
+import BPLogo from '@/assets/images/bp-logo.png';
+
+export default function Home() {
+  return (
+    <main>
+      <p>page :0</p>
+    </main>
+  );
+}
