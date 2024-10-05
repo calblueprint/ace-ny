9b605b6248887c99832f6f8846c283b810423bdc
Neha <neha_h@berkeley.edu>
2024-10-05 16:52:05 -0700

    [chore] fix styling issues

--- styles/colors.ts
+++ styles/colors.ts
@@ -2,16 +2,15 @@ const COLORS = {
   navy: '#2E3A59',
   blue: '#4974E0',
   lightBlue: '92ACED',
-  green: '#0E7B30', 
+  green: '#0E7B30',
   white: '#FFFFFF',
   orange: '#D4874D',
-  grey: '#636363', 
+  grey: '#636363',
   lightGreen: 'E8FFEF',
   veryLightGrey: 'F4F4F4',
   lightOrange: 'FFF9DF',
   black: '#000000',
-  lightGrey: 'D9D9D9', 
-  
+  lightGrey: 'D9D9D9',
 };
 
 export default COLORS;
