58419e2b8ea0a1eff7364e1d2df6a35d5d9dd5c3
Ethan Tam <ethantam@berkeley.edu>
2024-10-06 00:52:17 -0700

    [feat] added google maps to home page

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -1,1 +1,45 @@
-import { Loader } from '@googlemaps/js-api-loader';
+'use client';
+
+import { useCallback, useState } from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '700px',
+  height: '700px',
+};
+
+const center = {
+  lat: 40.7128,
+  lng: -74.006,
+};
+
+const zoom = 7;
+
+export default function Map() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+  });
+
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+
+  const onLoad = useCallback((map: google.maps.Map) => {
+    map.setCenter(center);
+    map.setZoom(zoom);
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    ></GoogleMap>
+  ) : (
+    <></>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { CSSProperties } from 'react';
 import Image from 'next/image';
 import BPLogo from '@/assets/images/bp-logo.png';
+import Map from './components/map';
 
 export default function Home() {
   return (
     <main style={mainStyles}>
-      <Image style={imageStyles} src={BPLogo} alt="Blueprint Logo" />
-      <p>Open up app/page.tsx to get started!</p>
+      <Map />
     </main>
   );
 }
