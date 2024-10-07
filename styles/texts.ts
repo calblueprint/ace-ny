1b5204e118f43e0025c86ad879a78ef9184cf746
Neha <neha_h@berkeley.edu>
2024-10-06 17:25:44 -0700

    [chore] lint errors

---
+++ styles/texts.ts
@@ -0,0 +1,145 @@
+import styled from 'styled-components';
+
+export const Heading1 = styled.h1`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 40px;
+  font-style: normal;
+  line-height: normal;
+`;
+
+export const Normal700Text = styled.div`
+  font-family: 'Coinbase Text', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const Heading1Bold = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+`;
+
+export const Heading2 = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 35px;
+  font-style: normal;
+  line-height: normal;
+`;
+
+export const Heading2Bold = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+`;
+
+export const Heading3 = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 30px;
+  font-style: normal;
+  line-height: normal;
+`;
+
+export const Heading3Bold = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const HeadingTemp = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const Heading4 = styled.div`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const Heading4Bold = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const Heading5 = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+`;
+
+export const Heading5Bold = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
+
+// done under
+export const Body1 = styled.p`
+  // output, status, category, filter by
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const Body1Bold = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+`;
+
+export const Body2 = styled.p`
+  // hydro, central ny
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 120%; /* 14.4px */
+`;
+
+export const Body2Bold = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120%; /* 14.4px */
+`;
+
+export const Body3 = styled.p`
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 9px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+`;
+
+/*
+export const StatusFont = styled.p`
+  font-family: "Coinbase Text", sans-serif;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 300;
+  line-height: 120%;  14.4px 
+`;*/
