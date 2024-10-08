2f59cbdaf4f9a7f185a233bbda9e5598d7e0accc
Justin Lee <jjunseol04@gmail.com>
2024-10-07 19:35:56 -0700

    Fixing merge conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import { CSSProperties } from 'react';
-import Image from 'next/image';
-import BPLogo from '@/assets/images/bp-logo.png';
 import ProjectModal from '@/components/ProjectModal';
 import Map from './components/map';
 
 export default function Home() {
   return (
     <main style={mainStyles}>
-      <Image style={imageStyles} src={BPLogo} alt="Blueprint Logo" />
       <ProjectModal
         project_name="RIPTIDE STORAGE"
         developer="ACE DevCo NC, LLC"
