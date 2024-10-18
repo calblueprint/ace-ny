1c5575da0c9bee6220f0c9951407fb7f17229816
Neha <neha_h@berkeley.edu>
2024-10-18 15:47:57 -0700

    [chore] rebasing

--- components/ProjectModal/styles.ts
+++ components/ProjectModal/styles.ts
@@ -79,20 +79,3 @@ export const closeButtonStyles: CSSProperties = {
 export const additionalInfoStyles: CSSProperties = {
   width: '315px',
 };
-
-export const projectContainerStyles: CSSProperties = {
-  position: 'relative', // Positions children absolutely within this container
-  width: '390px',
-  height: '300px', // Adjust height to fit content + search bar
-  overflow: 'hidden',
-};
-
-export const projectImageStyles: CSSProperties = {
-  position: 'absolute', // Allows the image to be placed behind other content
-  top: '0',
-  left: '0',
-  width: '100%',
-  height: '100%',
-  objectFit: 'cover', // Ensures the image fills the container without distortion
-  zIndex: 1, // Lower z-index to push the image behind
-};
