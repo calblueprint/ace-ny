2cb7a37b48e609ff72068e0081a0a887006a7a7e
Deena <156876027+deenasun@users.noreply.github.com>
2024-10-23 23:21:31 -0700

    [feat] Connecting NYISO and NYSERDA to Populate Database (#29)
    * [feat] added check_status function for project queries from NYSERDA
    
    * [feat] added util helper modules to check project status and use google geocoding api to retrieve latitude and longitude
    
    * pull and rebase from main
    
    * [refactor] put query calls for nyserda/nyiso into functions, added additional filtered fields, created database.py
    
    * [feat] database function to insert/update nyserda large data to supabase
    
    * [feat] functions to insert projects into database, geocodio api to retrieve senate/congressional districts
    
    * [feat] nyiso_to_database function to cross-reference based on interconnection queue number
    
    * [feat] map renewable energy tech abbreviation from NYISO to readable string if there exists a suitable mapping
    
    * [chore] created requirements.txt file for python dependencies
    
    * [fix] replace NaN values in nyiso df with None
    
    * [chore] updated documentation
    
    * [chore] removed cd field for geocode reverse

--- api/webscraper/scraper.py
+++
@@ -1,7 +0,0 @@
-import requests
-import pandas as pd
-from io import BytesIO
-
-nyiso = requests.get('https://www.nyiso.com/documents/20142/1407078/NYISO-Interconnection-Queue.xlsx')
-nyiso_data = nyiso.content
-nyiso_df = pd.read_excel(BytesIO(nyiso_data))
