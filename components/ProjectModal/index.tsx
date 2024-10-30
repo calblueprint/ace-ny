b031aeecb6a989e9ea00c02a0893dc80450d5b5e
nehaahussain <67764423+nehaahussain@users.noreply.github.com>
2024-10-30 13:48:54 -0700

    [chore] Styling of Project Modal (w/o KDMs) (#45)
    * [chore] styled components change
    
    * [feat] styling w/o info fix
    
    * [chore] styling
    
    * [feat] done with styling
    
    * [chore] fixed comments
    
    * [chore] added icon packages
    
    * added new font
    
    * [feat] addressed comments
    
    * [feat] small changes
    
    ---------
    
    Co-authored-by: Neha <neha_h@berkeley.edu>
    Co-authored-by: Monique <nica14101@gmail.com>

--- components/ProjectModal/index.tsx
+++ components/ProjectModal/index.tsx
@@ -1,10 +1,34 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { FiX, FiZap } from 'react-icons/fi';
 import Modal from 'react-modal';
 import { queryProjectbyId } from '../../api/supabase/queries/query';
+import {
+  AccentText1,
+  AccentText2,
+  BodyText1,
+  Heading1,
+  TagText1,
+} from '../../styles/texts';
 import { Project } from '../../types/schema';
-import * as styles from './styles';
+import {
+  AdditionalInfo,
+  AdditionalText,
+  CloseButton,
+  DetailsContainer,
+  Developer,
+  Divider,
+  modalContentStyles,
+  modalOverlayStyles,
+  ProjectDetails,
+  ProjectFilter,
+  ProjectFilterWrapper,
+  projectImageStyles,
+  ProjectName,
+  ProjectOverview,
+  ProjectSize,
+} from './styles';
 
 export default function ProjectModal({
   project_id,
@@ -49,35 +73,53 @@ export default function ProjectModal({
       <Modal
         isOpen={openFirst}
         style={{
-          overlay: styles.modalOverlayStyles,
-          content: styles.modalContentStyles,
+          overlay: modalOverlayStyles,
+          content: modalContentStyles,
         }}
       >
-        <div style={styles.projectContainerStyles}>
+        <ProjectDetails>
           <img
             src={project_image ? project_image : ''}
             alt="Project Image"
-            style={styles.projectImageStyles}
+            style={projectImageStyles}
           />
-          <div style={styles.searchBarStyles}>Search</div>
-          <div style={styles.projectNameStyles}>
-            <div style={styles.developerStyles}>
-              Developer - {developer}
-              <button onClick={closeModal} style={styles.closeButtonStyles}>
-                Close
-              </button>
-            </div>
-            <div>{project_name}</div>
-            <div>{project_status}</div>
-            <div>{renewable_energy_technology}</div>
-          </div>
-        </div>
-        <div>{size}</div>
-        <div style={styles.additionalInfoStyles}>
-          DETAILS
-          <br></br>
-          {additional_information}
-        </div>
+          <ProjectOverview>
+            <Developer>
+              <BodyText1>Developer ‣ {developer}</BodyText1>
+              <CloseButton onClick={closeModal}>
+                <FiX size={20} color="#000" />
+              </CloseButton>
+            </Developer>
+            <ProjectName>
+              <Heading1>{project_name?.toUpperCase()}</Heading1>
+            </ProjectName>
+            <ProjectFilterWrapper>
+              <ProjectFilter>
+                <TagText1>{project_status}</TagText1>
+              </ProjectFilter>
+              <ProjectFilter>
+                <TagText1>{renewable_energy_technology}</TagText1>
+              </ProjectFilter>
+            </ProjectFilterWrapper>
+          </ProjectOverview>
+          <ProjectSize>
+            <AccentText1>
+              <FiZap size={42} />
+              {size}
+            </AccentText1>
+            <AccentText2>MW / Mo</AccentText2>
+          </ProjectSize>
+          <Divider />
+          <AdditionalInfo>
+            <DetailsContainer>
+              <BodyText1>DETAILS</BodyText1>
+              <Divider />
+            </DetailsContainer>
+            <AdditionalText>
+              <BodyText1>{additional_information}</BodyText1>
+            </AdditionalText>
+          </AdditionalInfo>
+        </ProjectDetails>
       </Modal>
     </div>
   );
--- components/ProjectModal/styles.ts
+++ components/ProjectModal/styles.ts
@@ -1,81 +1,129 @@
 import { CSSProperties } from 'react';
+import styled from 'styled-components';
+import COLORS from '../../styles/colors';
 
 export const modalOverlayStyles: CSSProperties = {
   width: '310px',
   height: '100%',
+  backgroundColor: 'transparent',
 };
 
 export const modalContentStyles: CSSProperties = {
   display: 'flex',
-  width: '360px',
-  height: '100%',
-  top: '0',
-  bottom: '0',
-  left: '0',
-  right: '0',
-  flexShrink: 0,
+  top: '85px',
+  left: '20px',
+  width: '356px',
+  height: '85%',
+  borderRadius: 'var(--Spacing-Small, 16px)',
+  border: '0.75px solid var(--WorldPeas-White, #fff)',
+  background:
+    'linear-gradient(180deg, rgba(250, 250, 250, 0.32) 0%, rgba(238, 238, 238, 0.65) 100%)',
+  backdropFilter: 'blur(7.5px)',
+  paddingTop: '0.625rem',
+  paddingBottom: '0.625rem',
+  boxSizing: 'border-box',
   flexDirection: 'column',
   alignItems: 'center',
 };
 
-export const projectContainerStyles: CSSProperties = {
-  position: 'relative',
-  width: '360px',
-  height: '300px',
-  overflow: 'hidden',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-};
+export const ProjectDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: var(--Spacing-Small, 16px);
+  height: 100%;
+  background: ${COLORS.white};
+  width: 340px;
+`;
 
 export const projectImageStyles: CSSProperties = {
   position: 'absolute',
-  width: '359px',
-  height: '269px',
+  width: '340px',
+  height: '250px',
   objectFit: 'cover',
+  borderRadius: '8px 8px 0px 0px',
 };
 
-export const searchBarStyles: CSSProperties = {
-  width: '310px',
-  height: '50px',
-  display: 'flex',
-  flexShrink: '0',
-  borderRadius: '8px',
-  border: '1px solid rgba(46, 58, 89, 0.50)',
-  background: 'var(--WorldPeas-White, #FFF)',
-  boxShadow: '0px 4px 5px 0px rgba(255, 255, 255, 0.25)',
-  alignItems: 'center',
-  justifyContent: 'center',
-  zIndex: 2,
-  position: 'relative',
-};
+export const ProjectOverview = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 316px;
+  height: 129px;
+  margin-top: 9.5rem;
+  border-radius: 8px;
+  background: ${COLORS.white};
+  box-shadow:
+    0px 2px 6px rgba(77, 87, 114, 0.08),
+    0px -2px 5px rgba(255, 255, 255, 0.1);
+  position: relative;
+  box-sizing: border-box;
+  padding: 1rem;
+`;
 
-export const projectNameStyles: CSSProperties = {
-  width: '315px',
-  height: '135px',
-  marginTop: '152px',
-  flexShrink: '0',
-  borderRadius: '8px',
-  background: 'var(--WorldPeas-White, #FFF)',
-  boxShadow:
-    '0px 2px 6px 0px rgba(77, 87, 114, 0.08), 0px -2px 5px 0px rgba(255, 255, 255, 0.10)',
-  position: 'relative',
-  padding: '10px',
-};
+export const Developer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-export const developerStyles: CSSProperties = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  zIndex: 2,
-};
+export const ProjectName = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  padding-bottom: 0.75rem;
+`;
 
-export const closeButtonStyles: CSSProperties = {
-  position: 'absolute',
-  top: '10px',
-  right: '10px',
-  zIndex: 3,
-};
+export const ProjectFilterWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
 
-export const additionalInfoStyles: CSSProperties = {
-  width: '315px',
-};
+export const ProjectFilter = styled.div`
+  border-radius: 100px;
+  border: 0.5px solid rgba(46, 58, 89, 0.25);
+  display: inline-flex;
+  height: 22px;
+  padding: 0.1rem 0.625rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CloseButton = styled.button`
+  box-shadow: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
+export const ProjectSize = styled.div`
+  display: flex;
+  align-items: baseline;
+  width: 260px;
+  padding-top: 1.2rem;
+  gap: 0.5rem;
+`;
+
+export const Divider = styled.hr`
+  width: 260px;
+  border: 0;
+  height: 1px;
+  margin: 0px 0;
+  background: rgba(46, 58, 89, 0.1);
+`;
+
+export const AdditionalInfo = styled.div`
+  width: 260px;
+  padding: 1.25rem;
+`;
+
+export const DetailsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  gap: 0.5rem;
+`;
+
+export const AdditionalText = styled.div`
+  padding-top: 1.25rem;
+`;
