de61d400229d2add9e6916fe8a93d1175bc452b4
Ethan Tam <ethantam@berkeley.edu>
2024-11-02 00:07:26 -0700

    [feat] infowindow persists on click but bug

--- api/maps/MarkerInfoWindow.tsx
+++ api/maps/MarkerInfoWindow.tsx
@@ -13,6 +13,7 @@ export const MarkerInfoWindow = ({
   projectDev,
   onMarkerClick,
   clusterer,
+  selectedProjectId,
 }: {
   position: { lat: number; lng: number };
   projectId: number;
@@ -23,21 +24,46 @@ export const MarkerInfoWindow = ({
     position: { lat: number; lng: number },
   ) => void;
   clusterer: MarkerClusterer | null;
+  selectedProjectId: number | null;
 }) => {
   const [markerRef, marker] = useAdvancedMarkerRef();
   const [infoWindowShown, setInfoWindowShown] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [hoveredProjectId, setHoveredProjectId] = useState<number | null>(null);
 
   // hovering over the marker will toggle the infowindow
   const handleMarkerEnter = useCallback(() => {
-    setInfoWindowShown(isShown => !isShown);
+    if (!infoWindowShown) {
+      setInfoWindowShown(true);
+    }
   }, []);
 
   // if the maps api closes the infowindow, we have to synchronize our state
-  const handleClose = useCallback(() => setInfoWindowShown(false), []);
+  const handleClose = useCallback(() => {
+    console.log('modalOpen in close: ', modalOpen);
+    if (!modalOpen) {
+      setInfoWindowShown(false);
+    }
+  }, [modalOpen]);
 
   const handleMarkerClick = () => {
     onMarkerClick(projectId, position);
+    setModalOpen(!modalOpen);
+    if (!modalOpen) {
+      setInfoWindowShown(true);
+    } else {
+      setInfoWindowShown(false);
+    }
   };
+
+  useEffect(() => {
+    // Close InfoWindow if another marker is selected
+    if (selectedProjectId !== projectId) {
+      setInfoWindowShown(false);
+    }
+    console.log(selectedProjectId, projectId);
+    console.log(infoWindowShown);
+  }, [selectedProjectId, projectId]);
 
   useEffect(() => {
     if (marker && clusterer) {
