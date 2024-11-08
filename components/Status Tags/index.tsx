aef04cdfc6d15259fc9743f722257f4de61467a3
Neha <neha_h@berkeley.edu>
2024-11-07 20:17:02 -0800

    [chore] file fixes

---
+++ components/Status Tags/index.tsx
@@ -0,0 +1,12 @@
+// import { CalendarIcon, GreenDotOperationalIcon, GreyDotInProgressIcon } from '../../assets/Status-Tag-Icons/icons';
+
+// export default function StatusTags({ status, cod}:
+//   {
+//     status: string | null;
+//     cod: string | null;
+//   }
+// ) {
+//   return (
+
+//   )
+// }
