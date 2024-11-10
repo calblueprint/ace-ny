7ecbbd0e46bb72fcf6e22f2c5b21bffa3f65454a
Ethan Tam <ethantam@berkeley.edu>
2024-11-10 04:06:03 -0800

    [style] added icon images

--- components/MapViewScreen/index.tsx
+++ components/MapViewScreen/index.tsx
@@ -7,7 +7,13 @@ import Map from '@/components/Map';
 import { SearchBar } from '@/components/SearchBar';
 import SVGIcon from '@/components/SVGIcon';
 import { Filter } from '@/types/schema';
+import EnergyStorage from '../../assets/DropdownIcons/EnergyStorage.svg';
+import Geothermal from '../../assets/DropdownIcons/Geothermal.svg';
+import Hydroelectric from '../../assets/DropdownIcons/Hydroelectric.svg';
+import LandbasedWind from '../../assets/DropdownIcons/LandbasedWind.svg';
+import OffshoreWind from '../../assets/DropdownIcons/OffshoreWind.svg';
 import PumpedStorage from '../../assets/DropdownIcons/PumpedStorage.svg';
+import SolarPower from '../../assets/DropdownIcons/SolarPower.svg';
 import { Project } from '../../types/schema';
 
 export default function MapViewScreen(props: { projects: Project[] | null }) {
@@ -26,17 +32,35 @@ export default function MapViewScreen(props: { projects: Project[] | null }) {
         {
           category: 'Source',
           options: [
-            { title: 'Land-based Wind', icon: <FaBolt /> },
-            { title: 'Hydroelectric', icon: <FaBolt /> },
-            { title: 'Offshore Wind', icon: <FaBolt /> },
-            { title: 'Solar Power', icon: <FaBolt /> },
-            { title: 'Geothermal', icon: <FaBolt /> },
+            {
+              title: 'Land-based Wind',
+              icon: <SVGIcon src={LandbasedWind} alt="Land-based Wind" />,
+            },
+            {
+              title: 'Hydroelectric',
+              icon: <SVGIcon src={Hydroelectric} alt="Hydroelectric" />,
+            },
+            {
+              title: 'Offshore Wind',
+              icon: <SVGIcon src={OffshoreWind} alt="Offshore Wind" />,
+            },
+            {
+              title: 'Solar Power',
+              icon: <SVGIcon src={SolarPower} alt="Solar Power" />,
+            },
+            {
+              title: 'Geothermal',
+              icon: <SVGIcon src={Geothermal} alt="Geothermal" />,
+            },
           ],
         },
         {
           category: 'Storage',
           options: [
-            { title: 'Energy Storage', icon: <FaBolt /> },
+            {
+              title: 'Energy Storage',
+              icon: <SVGIcon src={EnergyStorage} alt="Energy Storage" />,
+            },
             {
               title: 'Pumped Storage',
               icon: <SVGIcon src={PumpedStorage} alt="Pumped Storage" />,
