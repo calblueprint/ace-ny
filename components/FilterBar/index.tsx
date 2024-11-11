7e2b3bcafd4bb4c58fd6db033ab976b9c19d7a37
Ethan Tam <ethantam@berkeley.edu>
2024-11-11 05:54:48 -0800

    cleaned up

--- components/FilterBar/index.tsx
+++ components/FilterBar/index.tsx
@@ -18,9 +18,9 @@ export const FilterBar = ({
   const [activeFilter, setActiveFilter] = useState<Filter | null>(null);
 
   const [selectedFilters, setSelectedFilters] = useState<Filters>({
-    statusCompleted: false, // Single-select ("in progress", "completed")
-    technology: [], // Multi-select
-    projectSize: { min: 0, max: 0 }, // Numeric range
+    statusCompleted: false,
+    technology: [],
+    projectSize: { min: 0, max: 0 },
     location: [],
   });
 
@@ -34,44 +34,19 @@ export const FilterBar = ({
   }, [activeFilter]);
 
   const handleTechnologyChange = (options: string[]) => {
-    console.log('HIII');
     setSelectedFilters(prevFilters => ({
       ...prevFilters,
       technology: options,
     }));
   };
-
-  // create a new component just for the technology dropdown
-  // once the button is clicked it disappears, and the dropdown appears + replaces where the button was at
-  // once the dropdown is closed, the button reappears
-  // slap the dropdown on top of the button, and (make the button invisible? not really needed tho)
-
-  //filterbackgroundstyles, filterbuttonstyles should disappear when the dropdown is open
-  // use filters.map to render the filter buttons, but not for the dropdown
-
-  // if activeFilter===filter, render the specific dropdown on top of the button
-  console.log('Filters', filters);
   return (
     <FilterContainerStyles>
-      {/* {filters.map(filter =>
-        activeFilter?.id === filter.id ? null : (
-          <FilterBackgroundStyles key={filter.label}>
-            <FilterButtonStyles
-              key={filter.label}
-              onClick={() => handleButtonClick(filter)}
-            >
-              {filter.icon}
-              {filter.label}
-              <RiArrowDropDownLine />
-            </FilterButtonStyles>
-          </FilterBackgroundStyles>
-        ),
-      )} */}
       {filters.map(filter => (
         <FilterBackgroundStyles
           key={filter.label}
           isActive={activeFilter?.id === filter.id}
         >
+          {/* Create rest of filter dropdowns in here */}
           {activeFilter?.id === filter.id ? (
             filter.id === 'technology' ? (
               <TechnologyDropdown
@@ -92,15 +67,6 @@ export const FilterBar = ({
           )}
         </FilterBackgroundStyles>
       ))}
-      {/* {activeFilter?.id === 'technology' && (
-        <div>
-          <TechnologyDropdown
-            selectedTechnologies={selectedFilters.technology}
-            setSelectedTechnologies={handleTechnologyChange}
-          />
-        </div>
-      )} */}
-      {/* Add more filter components here */}
     </FilterContainerStyles>
   );
 };
