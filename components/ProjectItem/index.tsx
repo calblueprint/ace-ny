75ea08981b7b476439d0f46c270668841abf47d6
Justin Lee <91563628+jjstnlee@users.noreply.github.com>
2024-11-12 03:05:08 -0800

    [feat] Direct Search Item Component (#62)
    * Added Project Item component
    
    * meep
    
    * fixed font issues kinda
    
    * minor fixes
    
    * chore: styling fixes (#64)
    
    * chore: styling fixes
    
    * adjusted favicon and page title
    
    * adjusted favicon and page title
    
    * [feat] Customized Icons for Renewable Energy Technology and Status (incl. Proposed COD) (#63)
    
    * [chore] added tags, files
    
    * [chore] file fixes
    
    * [chore] file fixes
    
    * [feat] finished status tags
    
    * [feat] did tech tags
    
    * [chore] rem
    
    * [feat] done?
    
    * [chore] small fixes
    
    * [chore] name change, cod
    
    * [feat] proposed styling
    
    * [chore] refactoring
    
    * [feat] fixed icons
    
    * [chore] changed geothermal, small fixes
    
    ---------
    
    Co-authored-by: Neha <neha_h@berkeley.edu>
    
    * Added Project Item component
    
    * meep
    
    * fixed font issues kinda
    
    * minor fixes
    
    * Used neha's energy type icons
    
    * changed svg properties to camelCase
    
    ---------
    
    Co-authored-by: Monique <63492974+itsliterallymonique@users.noreply.github.com>
    Co-authored-by: nehaahussain <67764423+nehaahussain@users.noreply.github.com>
    Co-authored-by: Neha <neha_h@berkeley.edu>

---
+++ components/ProjectItem/index.tsx
@@ -0,0 +1,167 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import {
+  queryDefaultImages,
+  queryProjectbyId,
+} from '@/api/supabase/queries/query';
+import { SmallSizeIcon } from '@/assets/Size-Icons/icons';
+import {
+  GreenDotOperationalIcon,
+  GreyDotInProgressIcon,
+} from '@/assets/Status-Tag-Icons/icons';
+import {
+  EnergyStorageIcon,
+  GeothermalIcon,
+  HydroelectricIcon,
+  LandBasedWindIcon,
+  OffshoreWindIcon,
+  PumpedStorageIcon,
+  SolarPvIcon,
+} from '@/assets/Technology-Tag-Icons/icons';
+import COLORS from '@/styles/colors';
+import { Heading2, TagText2 } from '@/styles/texts';
+import { Project } from '@/types/schema';
+import ProjectModal from '../ProjectModal';
+import {
+  projectImageStyles,
+  ProjectInfo,
+  ProjectName,
+  ProjectSize,
+  ProjectSizeAndType,
+  ProjectStatus,
+  ProjectType,
+  StyledProjectItem,
+} from './styles';
+
+export default function ProjectItem({ project_id }: { project_id: number }) {
+  const [project, setProject] = useState<Project | null>(null);
+  const [defaultImage, setDefaultImage] = useState<string | null>(null);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    queryProjectbyId(project_id).then(data => {
+      setProject(data);
+    });
+  }, [project_id]);
+
+  useEffect(() => {
+    // Fetch default image when project data is available
+    const fetchDefaultImage = async () => {
+      if (!project?.project_image && project?.renewable_energy_technology) {
+        try {
+          const fetchedImage = await queryDefaultImages(
+            project.renewable_energy_technology,
+          );
+          setDefaultImage(fetchedImage.default_image);
+        } catch (error) {
+          console.error('Error fetching default image:', error);
+        }
+      }
+    };
+    fetchDefaultImage();
+  }, [project]);
+
+  const {
+    // id,
+    project_name,
+    renewable_energy_technology,
+    size,
+    // developer,
+    // longitude,
+    // latitude,
+    project_status,
+    // county,
+    // town,
+    // region,
+    // state_senate_district,
+    // assembly_district,
+    project_image,
+    // additional_information,
+    // key_development_milestones,
+    // proposed_cod,
+    // approved
+  } = project || {};
+
+  const getProjectImageSrc = () => {
+    return project_image || defaultImage || '';
+  };
+
+  // Sets status label to "Operational" or "In Progress"
+  let projectStatus = project_status;
+  if (project_status !== 'Operational') {
+    projectStatus = 'In Progress';
+  }
+
+  // Sets status icon to OperationalIcon or InProgressIcon
+  let statusIcon = <GreenDotOperationalIcon />;
+  if (project_status !== 'Operational') {
+    statusIcon = <GreyDotInProgressIcon />;
+  }
+
+  const projectImageAlt = project_image
+    ? `${project_name} project image`
+    : defaultImage
+      ? `${renewable_energy_technology} default image`
+      : 'No image available';
+
+  const energyTypeIconMap: { [key: string]: JSX.Element } = {
+    'Land-Based Wind': <LandBasedWindIcon fill={COLORS.skyBlue} />,
+    'Solar PV': <SolarPvIcon fill={COLORS.solarYellow} />,
+    Hydroelectric: <HydroelectricIcon fill={COLORS.frenchBlue} />,
+    'Offshore Wind': (
+      <OffshoreWindIcon fill={COLORS.electricBlue} stroke={COLORS.navy} />
+    ),
+    Geothermal: <GeothermalIcon fill={COLORS.earthyGreen} />,
+    'Energy Storage': (
+      <EnergyStorageIcon fill={COLORS.teal} stroke={COLORS.white} />
+    ),
+    'Pumped Storage': <PumpedStorageIcon fill={COLORS.cyanBlue} />,
+  };
+
+  const handleProjectClick = () => {
+    setModalOpen(true);
+  };
+
+  if (modalOpen) {
+    return (
+      <ProjectModal
+        project_id={project_id}
+        closeModal={() => setModalOpen(false)}
+        openFirst={true}
+      />
+    );
+  }
+
+  return (
+    <StyledProjectItem onClick={handleProjectClick}>
+      <ProjectInfo>
+        <Heading2>
+          <ProjectName>{project_name?.toUpperCase()}</ProjectName>
+        </Heading2>
+        <ProjectStatus>
+          {statusIcon}
+          <TagText2>{projectStatus}</TagText2>
+        </ProjectStatus>
+        <ProjectSizeAndType>
+          <ProjectSize>
+            <SmallSizeIcon />
+            <TagText2>{size} MW</TagText2>
+          </ProjectSize>
+          <ProjectType>
+            {energyTypeIconMap[renewable_energy_technology ?? '']}
+            <TagText2>{renewable_energy_technology}</TagText2>
+          </ProjectType>
+        </ProjectSizeAndType>
+      </ProjectInfo>
+      <Image
+        src={getProjectImageSrc()}
+        alt={projectImageAlt}
+        width={340}
+        height={250}
+        style={projectImageStyles}
+      />
+    </StyledProjectItem>
+  );
+}
--- styles/colors.ts
+++ styles/colors.ts
@@ -1,11 +1,12 @@
 const COLORS = {
   lightOrange: '#FFF9DF',
   orange: '#D4874D',
-  yellow: '#FFBE54',
+  solarYellow: '#FFBE54',
   lightGreen: '#E8FFEF',
   green: '#0E7B30',
   earthyGreen: '#4D8B31',
   navy: '#2E3A59',
+  navy75: '#626C83',
   electricBlue: '#4974E0',
   lightBlue: '#92ACED',
   teal: '#4896BC',
