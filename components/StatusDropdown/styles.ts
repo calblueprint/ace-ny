82282ed675c35ce74ac7ac8c1b4ee59d17db3b35
Neha <neha_h@berkeley.edu>
2024-11-15 19:16:58 -0800

    [chore] fixed styling

--- components/StatusDropdown/styles.ts
+++ components/StatusDropdown/styles.ts
@@ -4,14 +4,14 @@ import COLORS from '../../styles/colors';
 export const FilterDropdownStyles = styled.div`
   position: relative;
   background: ${COLORS.white};
-  width: 11rem;
+  width: 10.5rem;
   border-radius: 0.5rem;
   padding-bottom: 0.5rem;
 `;
 
 export const OptionTitleStyles = styled.p<{ color: string }>`
-  background: ${({ color }) => `${color}15`};
-  border-radius: 0.5rem;
+  background: ${({ color }) => `${color}10`};
+  border-radius: 0.3rem;
   padding: 0.1rem 0.4rem;
   display: inline-block;
 `;
@@ -21,7 +21,7 @@ export const CheckboxStyles = styled.input`
 `;
 
 export const ApplyButtonStyles = styled.button<{ isActive: boolean }>`
-  width: 10.8125rem;
+  width: 8rem;
   height: 1.5rem;
   flex-shrink: 0;
   border-radius: 0.25rem;
@@ -29,16 +29,12 @@ export const ApplyButtonStyles = styled.button<{ isActive: boolean }>`
     isActive
       ? COLORS.electricBlue
       : 'rgba(73, 116, 224, 0.50)'}; // Active vs Inactive color
-  margin-top: 1rem;
-  margin-top: 1rem;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: ${({ isActive }) => (isActive ? 'pointer' : 'not-allowed')};
   border: none;
-  margin-bottom: 0.5rem;
-  width: 88%;
-  margin: 1rem auto 0.5rem auto;
+  margin: 1.4rem auto 0.5rem auto;
   pointer-events: ${({ isActive }) => (isActive ? 'auto' : 'none')};
 `;
 
@@ -55,6 +51,7 @@ export const ButtonStyles = styled.button`
 export const ExitStyles = styled.div`
   margin-left: auto;
   margin-right: 1.1rem;
+  margin-bottom: auto;
 `;
 
 export const ButtonWithIconStyles = styled.div`
@@ -63,6 +60,7 @@ export const ButtonWithIconStyles = styled.div`
   gap: 0.5rem;
   margin-left: 1rem;
   padding-top: 1rem;
+  margin-bottom: -0.4rem;
   cursor: pointer;
 `;
 
@@ -70,10 +68,10 @@ export const CheckboxContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 0.5rem;
-  margin-bottom: -0.8rem;
+  margin-bottom: -1.5rem;
   margin-left: 1rem;
 `;
 export const IconStyles = styled.div`
-  width: '3rem',
-  height: '3rem',
+  width: 3rem;
+  height: 3rem;
 `;
