529b91e03f3e9a7ba6b7a5642daa5ffd92af478d
Nathan Huynh <nathanhuynh102@gmail.com>
2025-03-05 22:42:44 -0800

    fixing prettier

--- components/DownloadData/index.tsx
+++ components/DownloadData/index.tsx
@@ -11,23 +11,29 @@ export default function DownloadData({
     const headers = Object.keys(filteredProjects[0]) as (keyof Project)[];
     console.log(headers);
     csvContent += headers.join(',') + '\n'; // add headers to csv
-    for (const project of filteredProjects) { // add each project to csv
+    for (const project of filteredProjects) {
+      // add each project to csv
       let row = '';
       for (const header of headers) {
-        if (header === 'key_development_milestones') { // destructuring array of objects to string
-          const milestonesToAdd = project[header]?.map((milestone) => {
+        if (header === 'key_development_milestones') {
+          // destructuring array of objects to string
+          const milestonesToAdd = project[header]?.map(milestone => {
             console.log(milestone);
-            return JSON.stringify(milestone).replace(/"/g, '""'); 
+            return JSON.stringify(milestone).replace(/"/g, '""');
           });
           row += `"${milestonesToAdd.join(',')}",`;
-        } else if (header === 'last_updated') { // destructuring object to string
-          const lastUpdatedStr = JSON.stringify(project[header]).replace(/"/g, '""'); 
+        } else if (header === 'last_updated') {
+          // destructuring object to string
+          const lastUpdatedStr = JSON.stringify(project[header]).replace(
+            /"/g,
+            '""',
+          );
           row += `"${lastUpdatedStr}",`;
         } else {
-          row += `${project[header]},`; 
+          row += `${project[header]},`;
         }
       }
-      csvContent += row.slice(0, -1) + '\n'; 
+      csvContent += row.slice(0, -1) + '\n';
     }
 
     // creates downloadable blob that downloads the csv file
