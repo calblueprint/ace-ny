31854c04fd3ebb420d1ac4457ce02f3460bc8bee
Nathan Huynh <nathanhuynh102@gmail.com>
2025-03-05 22:21:56 -0800

    fixed CSV parsing for key_development_milestones and last_updated header

--- components/DownloadData/index.tsx
+++ components/DownloadData/index.tsx
@@ -6,17 +6,31 @@ export default function DownloadData({
 }: {
   filteredProjects: Project[];
 }) {
-  // some logic here
-
   const downloadFilteredProjects = async () => {
     let csvContent = '';
     const headers = Object.keys(filteredProjects[0]) as (keyof Project)[];
-    csvContent += headers.join(',') + '\n';
-    for (const project of filteredProjects) {
-      const row = headers.map(header => project[header]);
-      csvContent += row.join(',') + '\n';
+    console.log(headers);
+    csvContent += headers.join(',') + '\n'; // add headers to csv
+    for (const project of filteredProjects) { // add each project to csv
+      let row = '';
+      for (const header of headers) {
+        if (header === 'key_development_milestones') { // destructuring array of objects to string
+          const milestonesToAdd = project[header]?.map((milestone) => {
+            console.log(milestone);
+            return JSON.stringify(milestone).replace(/"/g, '""'); 
+          });
+          row += `"${milestonesToAdd.join(',')}",`;
+        } else if (header === 'last_updated') { // destructuring object to string
+          const lastUpdatedStr = JSON.stringify(project[header]).replace(/"/g, '""'); 
+          row += `"${lastUpdatedStr}",`;
+        } else {
+          row += `${project[header]},`; 
+        }
+      }
+      csvContent += row.slice(0, -1) + '\n'; 
     }
 
+    // creates downloadable blob that downloads the csv file
     const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
     const url = URL.createObjectURL(blob);
     const link = document.createElement('a');
