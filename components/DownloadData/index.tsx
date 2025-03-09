6cad788eacae07a66a472a2d65dcbae282c5331b
Nathan Huynh <124105634+nathanhuynhers@users.noreply.github.com>
2025-03-09 16:14:21 -0700

    [Download Data] Button that downloads filtered projects into a CSV file (#123)
    * add DownloadData button with functionality to download filtered projects into a CSV file
    
    * add prettier to format correctly
    
    * fixed CSV parsing for key_development_milestones and last_updated header
    
    * fixing prettier
    
    * remove console.logs
    
    * removed async
    
    ---------
    
    Co-authored-by: Justin Lee <jjunseol04@gmail.com>

---
+++ components/DownloadData/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Project } from '@/types/schema';
+
+export default function DownloadData({
+  filteredProjects,
+}: {
+  filteredProjects: Project[];
+}) {
+  const downloadFilteredProjects = () => {
+    let csvContent = '';
+    const headers = Object.keys(filteredProjects[0]) as (keyof Project)[];
+    csvContent += headers.join(',') + '\n'; // add headers to csv
+    for (const project of filteredProjects) {
+      // add each project to csv
+      let row = '';
+      for (const header of headers) {
+        if (header === 'key_development_milestones') {
+          // destructuring array of objects to string
+          const milestonesToAdd = project[header]?.map(milestone => {
+            return JSON.stringify(milestone).replace(/"/g, '""');
+          });
+          row += `"${milestonesToAdd.join(',')}",`;
+        } else if (header === 'last_updated') {
+          // destructuring object to string
+          const lastUpdatedStr = JSON.stringify(project[header]).replace(
+            /"/g,
+            '""',
+          );
+          row += `"${lastUpdatedStr}",`;
+        } else {
+          row += `${project[header]},`;
+        }
+      }
+      csvContent += row.slice(0, -1) + '\n';
+    }
+
+    // creates downloadable blob that downloads the csv file
+    const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.setAttribute('href', url);
+    link.setAttribute('download', 'projects.csv');
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+  return <button onClick={downloadFilteredProjects}>Download Projects</button>;
+}
