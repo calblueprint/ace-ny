bf4730f010a24912f67bae5d8fac77ba2edfb1ec
Ethan Gu <ethangu4@gmail.com>
2025-03-09 23:41:57 -0700

    finished sprint 126

--- components/ProjectsListingModal/index.tsx
+++ components/ProjectsListingModal/index.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { useState } from 'react';
 import Modal from 'react-modal';
-import { OpenIcon } from '@/assets/KDM-Icons/icons';
+import { CloseIcon, OpenIcon } from '@/assets/KDM-Icons/icons';
 import { GlobeIcon } from '@/assets/Project-Icons/icons';
 import { SubHeading2 } from '@/styles/texts';
 import { Project } from '../../types/schema';
@@ -9,6 +10,8 @@ import ProjectItem from '../ProjectItem';
 import { SearchBar } from '../SearchBar';
 import {
   AllProjectsHeader,
+  DropdownItem,
+  DropdownMenu,
   Headers,
   ModalContents,
   modalContentStyles,
@@ -31,6 +34,10 @@ export default function ProjectsListingModal({
   searchTerm: string | null;
   setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
 }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setIsDropdownOpen(prev => !prev);
+
   const projectItems = projects?.map((project: Project) => {
     return (
       <ProjectItem
@@ -63,10 +70,28 @@ export default function ProjectsListingModal({
               <GlobeIcon width={'0.5625rem'} height={'0.5625rem'} />
               <SubHeading2>ALL PROJECTS</SubHeading2>
             </AllProjectsHeader>
-            <SortByButton onClick={() => {}}>
-              <SubHeading2>SORT BY</SubHeading2>
-              <OpenIcon width={'10'} height={'14'} />
-            </SortByButton>
+            <div style={{ position: 'relative' }}>
+              <SortByButton onClick={toggleDropdown}>
+                <SubHeading2>SORT BY</SubHeading2>
+                {isDropdownOpen ? (
+                  <CloseIcon />
+                ) : (
+                  <OpenIcon width={'10'} height={'14'} />
+                )}
+              </SortByButton>
+              {isDropdownOpen && (
+                <DropdownMenu>
+                  <DropdownItem onClick={() => {}}>Name A-Z</DropdownItem>
+                  <DropdownItem onClick={() => {}}>Name Z-A</DropdownItem>
+                  <DropdownItem onClick={() => {}}>Size Ascending</DropdownItem>
+                  <DropdownItem onClick={() => {}}>
+                    Size Descending
+                  </DropdownItem>
+                  <DropdownItem onClick={() => {}}>COD Ascending</DropdownItem>
+                  <DropdownItem onClick={() => {}}>COD Descending</DropdownItem>
+                </DropdownMenu>
+              )}
+            </div>
           </Headers>
           <ProjectItemsDiv>{projectItems}</ProjectItemsDiv>
         </ModalContents>
--- components/ProjectsListingModal/styles.ts
+++ components/ProjectsListingModal/styles.ts
@@ -70,15 +70,63 @@ export const ProjectItemsDiv = styled.div`
 
 export const SortByButton = styled.div`
   display: flex;
+  cursor: pointer;
   flex-direction: row;
+  align-items: center;
   background: ${COLORS.navy30};
   padding: 0.2rem 0.5rem;
   border-radius: 1.25rem;
   gap: 0.3rem;
+
+  margin-bottom: 3px;
 `;
 
 export const Headers = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
+  width: 102%;
+`;
+
+export const DropdownMenu = styled.div`
+  display: flex;
+  position: absolute;
+  width: 120px;
+  height: 152px;
+  flex-direction: column;
+  padding: 6px 8px;
+  align-items: flex-start;
+  gap: 4px;
+  flex-shrink: 0;
+  border-radius: 5px;
+  border-right: 0.5px solid rgba(46, 58, 89, 0.08);
+  border-bottom: 1.5px solid rgba(46, 58, 89, 0.08);
+  border-left: 0.5px solid rgba(46, 58, 89, 0.08);
+  background: #fafafb;
+  box-shadow: 0px -2px 5px 0px #fff;
+
+  z-index: 3;
+  right: 0;
+`;
+
+export const DropdownItem = styled.button`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: 2px 6px 2px 4px;
+  gap: 10px;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  color: var(--navy, rgba(46, 58, 89, 0.76));
+  font-family: 'Coinbase Text', sans-serif;
+  font-size: 0.688rem;
+  letter-spacing: 0.22px;
+
+  &:hover {
+    background: rgba(73, 116, 224, 0.05);
+    color: var(--not-ace-blue, #4974e0);
+    border-radius: 5px;
+  }
 `;
