b57dc2a6e24848558628ac4638704a754fcb5c15
Neha <neha_h@berkeley.edu>
2025-04-13 12:22:25 -0700

    added legend

--- components/LegendButton/index.tsx
+++ components/LegendButton/index.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import { InfoModalIcon } from '@/assets/InfoModal-Icons/icons';
 import { LegendButtonIcon } from '@/assets/Legend-Icons/icons';
-import COLORS from '@/styles/colors';
-import {
-  EnergyStorageIcon,
-  GeothermalIcon,
-  HydroelectricIcon,
-  LandBasedWindIcon,
-  OffshoreWindIcon,
-  PumpedStorageIcon,
-  SolarPvIcon,
-} from '../../assets/Technology-Tag-Icons/icons';
-import {
-  HeaderStyles,
-  InfoModalButtonBackground,
-  InfoModalLegendContent,
-  InfoModalStyles,
-  InfoModalWrapper,
-  TechnologyStyles,
-  TechnologyWrapperStyles,
-  TitleStyles,
-} from './styles';
+import Legend from '../Legend';
+import { LegendButtonBackground, LegendButtonWhiteBackground } from './styles';
 
 interface InfoModalButtonProps {
   aggFilterisVisible: boolean;
@@ -51,91 +32,12 @@ export default function LegendButton({
 
   return (
     <>
-      {legendIsVisible && (
-        <InfoModalStyles>
-          <InfoModalWrapper>
-            <InfoModalLegendContent>
-              <HeaderStyles>LEGEND</HeaderStyles>
-
-              <TitleStyles>TECHNOLOGY SOURCE</TitleStyles>
-
-              <TechnologyWrapperStyles>
-                <TechnologyStyles>
-                  <LandBasedWindIcon
-                    fill={COLORS.skyBlue}
-                    width="9.811px"
-                    height="13px"
-                  ></LandBasedWindIcon>
-                  Land-Based Wind
-                </TechnologyStyles>
-
-                <TechnologyStyles>
-                  <HydroelectricIcon
-                    fill={COLORS.frenchBlue}
-                    width="12px"
-                    height="9px"
-                  ></HydroelectricIcon>
-                  Hydroelectric
-                </TechnologyStyles>
-
-                <TechnologyStyles>
-                  <OffshoreWindIcon
-                    fill={COLORS.electricBlue}
-                    stroke={COLORS.navy}
-                    width="10.547px"
-                    height="13px"
-                  ></OffshoreWindIcon>
-                  Offshore Wind
-                </TechnologyStyles>
-
-                <TechnologyStyles>
-                  <SolarPvIcon
-                    fill={COLORS.solarYellow}
-                    width="13.112px"
-                    height="13px"
-                  ></SolarPvIcon>
-                  Solar PV
-                </TechnologyStyles>
-
-                <TechnologyStyles>
-                  <GeothermalIcon
-                    fill={COLORS.earthyGreen}
-                    width="11px"
-                    height="11px"
-                  ></GeothermalIcon>
-                  Geothermal
-                </TechnologyStyles>
-              </TechnologyWrapperStyles>
-
-              <TitleStyles>TECHNOLOGY STORAGE</TitleStyles>
-
-              <TechnologyWrapperStyles>
-                <TechnologyStyles>
-                  <EnergyStorageIcon
-                    fill={COLORS.teal}
-                    stroke={COLORS.white}
-                    width="12.8px"
-                    height="8.4px"
-                  ></EnergyStorageIcon>
-                  Energy Storage
-                </TechnologyStyles>
-
-                <TechnologyStyles>
-                  <PumpedStorageIcon
-                    fill={COLORS.cyanBlue}
-                    width="11.815px"
-                    height="10px"
-                  ></PumpedStorageIcon>
-                  Pumped Storage
-                </TechnologyStyles>
-              </TechnologyWrapperStyles>
-            </InfoModalLegendContent>
-          </InfoModalWrapper>
-        </InfoModalStyles>
-      )}
-      <InfoModalButtonBackground onClick={handleClick}>
-        <LegendButtonIcon />
-      </InfoModalButtonBackground>
+      {legendIsVisible && <Legend />}
+      <LegendButtonBackground onClick={handleClick}>
+        <LegendButtonWhiteBackground>
+          <LegendButtonIcon />
+        </LegendButtonWhiteBackground>
+      </LegendButtonBackground>
     </>
   );
 }
--- components/LegendButton/styles.ts
+++ components/LegendButton/styles.ts
@@ -1,8 +1,6 @@
 import { styled } from 'styled-components';
-import COLORS from '@/styles/colors';
-import { CoinbaseMono, CoinbaseSans, CoinbaseText } from '@/styles/fonts';
 
-export const InfoModalButtonBackground = styled.div`
+export const LegendButtonBackground = styled.div`
   position: relative;
   display: flex;
   justify-content: center;
@@ -19,141 +17,15 @@ export const InfoModalButtonBackground = styled.div`
   cursor: pointer;
 `;
 
-export const InfoModalStyles = styled.div`
-  position: absolute;
-  bottom: 60px;
-  right: 5px;
-  width: 14.6875rem;
-  height: 20.625rem;
-  padding: 16px 16px;
-  justify-content: center;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-radius: 12px;
-  background: #fff;
-  box-shadow:
-    0px 16px 20px 0px rgba(46, 58, 89, 0.1),
-    0px 1px 1px 0px rgba(46, 58, 89, 0.15);
-  z-index: 100;
-  word-wrap: break-word;
-`;
-
-export const InfoModalText = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  color: ${COLORS.navy75};
-  font-family: ${CoinbaseText.style};
-  font-size: 0.75rem;
-  font-style: normal;
-  font-weight: 250;
-  line-height: 16px;
-  padding: 5px 16px;
-  white-space: normal;
-  display: inline;
-  padding-bottom: 15px;
-  margin-top: -5px;
-`;
-
-export const InfoModalBoldedText = styled.div`
-  color: ${COLORS.electricBlue};
-  font-family: ${CoinbaseText.style};
-  font-size: 0.75rem;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 16px;
-  display: inline;
-`;
-
-export const InfoModalBoldedUnderlinedText = styled(InfoModalBoldedText)`
-  text-decoration: underline;
-  cursor: pointer;
-`;
-
-export const InfoModalTriangleStyles = styled.div`
-  position: absolute;
-  width: 1.5625rem;
-  height: 0.7719rem;
-  flex-shrink: 0;
-  z-index: 100;
-  bottom: 53px;
-  right: 15px;
-`;
-
-export const InfoModalRightArrowStyles = styled.div`
-  width: 0.7513rem;
-  height: 0.4641rem;
-  fill: ${COLORS.navy50};
-  cursor: pointer;
-`;
-
-export const InfoModalLeftArrowStyles = styled(InfoModalRightArrowStyles)`
-  rotate: 180deg;
+export const LegendButtonWhiteBackground = styled.div`
   position: relative;
-  top: 10px;
-  right: 3px;
-`;
-
-export const InfoModalCircleWrapper = styled.div`
   display: flex;
-  align-items: center;
-  gap: 7px;
-  z-index: 100;
-  position: absolute;
-  bottom: 20px;
-  left: 50%;
-  transform: translateX(-50%);
-`;
-
-export const HeaderStyles = styled.div`
-  color: ${COLORS.navy85};
-  font-family: ${CoinbaseSans.style};
-  font-size: 0.875rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-`;
-
-export const TitleStyles = styled.div`
-  color: ${COLORS.navy50};
-  font-family: ${CoinbaseMono.style};
-  font-size: 0.75rem;
-  font-style: normal;
-  font-weight: 400;
-  padding-top: 10px;
-  padding-bottom: 12px;
-`;
-
-export const TechnologyStyles = styled.div`
-  display: flex;
-  gap: 10px;
-  color: ${COLORS.navy75};
-  font-family: ${CoinbaseText.style};
-  font-size: 0.75rem;
-  font-style: normal;
-  font-weight: 300;
-  line-height: normal;
-  align-items: center;
   justify-content: center;
-  padding-left: 10px;
-  padding-bottom: 4px;
-`;
-
-export const TechnologyWrapperStyles = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 8px;
-  padding-bottom: 10px;
-`;
-
-export const InfoModalLegendContent = styled.div`
-  width: 11.9375rem;
-  padding: 8px 12px;
-`;
-
-export const InfoModalWrapper = styled.div`
-  display: flex;
   align-items: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background: #fff;
+  backdrop-filter: blur(7.5px);
+  cursor: pointer;
 `;
