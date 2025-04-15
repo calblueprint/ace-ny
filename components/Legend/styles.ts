45c527240310e42fb342340eb3d793c35cf385e8
nehaahussain <67764423+nehaahussain@users.noreply.github.com>
2025-04-14 19:31:59 -0700

    Restyle Info Modal + Add Legend (#161)
    * infomodal done
    
    * added legend
    
    * lint errors
    
    * small changes
    
    ---------
    
    Co-authored-by: Neha <neha_h@berkeley.edu>

---
+++ components/Legend/styles.ts
@@ -0,0 +1,76 @@
+import { styled } from 'styled-components';
+import COLORS from '@/styles/colors';
+import { CoinbaseMono, CoinbaseText } from '@/styles/fonts';
+
+export const LegendStyles = styled.div`
+  position: absolute;
+  bottom: 60px;
+  right: 60px;
+  width: 19.85rem;
+  padding: 16px;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 12px;
+  background: #fff;
+  box-shadow:
+    0px 16px 20px 0px rgba(46, 58, 89, 0.1),
+    0px 1px 1px 0px rgba(46, 58, 89, 0.15);
+  z-index: 100;
+  word-wrap: break-word;
+`;
+
+export const LegendTriangleStyles = styled.div`
+  position: absolute;
+  width: 1.5625rem;
+  height: 0.7719rem;
+  flex-shrink: 0;
+  z-index: 100;
+  bottom: 53px;
+  right: 71px;
+`;
+
+export const TitleStyles = styled.div`
+  color: ${COLORS.navy50};
+  font-family: ${CoinbaseMono.style};
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 400;
+`;
+
+export const LineStyles = styled.div`
+  padding-right: 10px;
+  padding-left: 10px;
+`;
+
+export const TechnologyStyles = styled.div`
+  display: flex;
+  gap: 10px;
+  color: ${COLORS.navy75};
+  font-family: ${CoinbaseText.style};
+  font-size: 0.75rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TechnologyWrapperStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 3px;
+`;
+
+export const LegendContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+`;
+
+export const LegendWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
