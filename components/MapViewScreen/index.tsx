aaf5c00c255dedb8917d608ec614cb1e7721ccdc
Ethan Gu <ethangu4@gmail.com>
2025-05-03 16:13:20 -0700

    one line fix

--- components/MapViewScreen/index.tsx
+++ components/MapViewScreen/index.tsx
@@ -251,7 +251,7 @@ export default function MapViewScreen({
         setSelectedProjectId={setSelectedProjectId}
       />
       <ProjectsListingModal
-        projects={filteredProjects}
+        projects={projects}
         map={map}
         setSelectedProjectId={setSelectedProjectId}
         searchTerm={searchTerm}
