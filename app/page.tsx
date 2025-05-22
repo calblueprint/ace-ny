9a469137133783b93070458b0d520014a9ce4118
Justin Lee <91563628+jjstnlee@users.noreply.github.com>
2025-05-22 01:58:53 -0700

    fixed image lagging (#190)

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,39 @@
 
 import { CSSProperties, useEffect, useState } from 'react';
 import MapViewScreen from '@/components/MapViewScreen';
-import queryProjects from '../api/supabase/queries/query';
+import queryProjects, {
+  queryDefaultImages,
+} from '../api/supabase/queries/query';
 import { Project } from '../types/schema';
 
 export default function Home() {
   const [projects, setProjects] = useState<Project[] | null>(null);
   const [filteredProjects, setFilteredProjects] = useState<Project[] | []>([]);
+  const [defaultImageMap, setDefaultImageMap] = useState<{
+    [key: string]: string;
+  }>({});
 
   useEffect(() => {
     queryProjects().then(data => {
       setProjects(data);
       setFilteredProjects(data);
     });
-  }, []);
+
+    queryDefaultImages().then(images => {
+      images.forEach(image => {
+        defaultImageMap[image.category] = image.default_image;
+      });
+      setDefaultImageMap(defaultImageMap);
+    });
+  }, [defaultImageMap]);
+
+  useEffect(() => {
+    projects?.forEach(project => {
+      project.project_image =
+        project.project_image ??
+        defaultImageMap[project.renewable_energy_technology];
+    });
+  }, [defaultImageMap, projects]);
 
   return (
     <main style={mainStyles}>
