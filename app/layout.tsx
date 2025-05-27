e89f14b45f14848ff0d455fa39b61e5230af7f09
Ethan Tam <ethantam@berkeley.edu>
2025-05-27 01:08:11 -0700

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 // font definitions
 const sans = Inter({
@@ -20,7 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={sans.className}>{children}</body>
+      <body className={sans.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
